b140d68ac8f46a7c5f070bd4347c779f39f65281
Jacob <jacobulysseslockwood@gmail.com>
2026-08-20 01:13:19 -0400

    update sanity, add appId

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -3,9 +3,10 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'hmslwij3',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
+    appId: 'fnwcgc6z2hn7x492eoll8md0',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
